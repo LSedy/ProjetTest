5257ca2e7ad06dd87340d6c4ca9f9f81c0c662d4
5Y2 <sedylaurent@gmail.com>
2019-12-20 09:54:32 +0100

    add rought  to project fort testing things

---
+++ ProjetTest/test/com/crm/formation/TestRought.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class TestRought {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+// Type entier
+		byte variableByte = 10;
+		short variableShort = 22000;
+		int variableInt = 123456789;
+		long variableLong = 943468432222L;
+
+// Type Réel
+		float variableFloat = 1.5F;
+		double variableDouble = 142.365544;
+
+// type caractère
+		char variableChar = 'E';
+
+//type booléen
+		boolean variableBoolean = true;
+
+		System.out.println("Ma variable byte vaut : " + variableByte);
+		System.out.println("Ma variable short à une valeur de : " + variableShort);
+		System.out.println("Ma  variable int equivaut à : " + variableInt);
+		System.out.println("Ma variable long est : " + variableLong);
+		System.out.println("Ma variable float vaut : " + variableFloat);
+		System.out.println("Ma variable double est équivalente à : " + variableDouble);
+		System.out.println("Ma variable char vaut : " + variableChar);
+		System.out.println("Ma variable boolean vaut : " + variableBoolean);
+
+	}
+
+}
