c33852c76937a971007d3c7ca1c06dca5fbdba18
5Y2 <sedylaurent@gmail.com>
2020-01-17 09:00:21 +0100

    add tableau

---
+++ ProjetTest/test/com/crm/formation/TableauTest.java
@@ -0,0 +1,23 @@
+package com.crm.formation;
+
+public class TableauTest {
+
+	public static void main(String[] args) {
+//		int[] tabInt;			int tableau[] = new int[50];
+//		tabInt = new int[5];
+		
+//		int[] tabInt1 = new int[5];
+//		System.out.println(tabInt1);
+		
+//		tabInt1[0] = 5
+//		System.out.println(tabInt1[0]);
+		
+//		System.out.println(tabInt1[1]);
+		
+		int tabInt2[] = {1,2,3,4,5};
+		System.out.println(tabInt2[4]);
+		
+		
+	}
+
+}
