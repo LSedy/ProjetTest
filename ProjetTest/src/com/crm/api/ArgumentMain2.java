319b767337c5943985f2dc7aba31e4e04ce7e61b
5Y2 <sedylaurent@gmail.com>
2020-01-24 08:59:49 +0100

    add clockwork args

---
+++ ProjetTest/src/com/crm/api/ArgumentMain2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+/**
+ * @author 5Y2
+ *
+ */
+public class ArgumentMain2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		if (args.length == 4) {
+			System.out.println("Welcome" + args[0] + " " + args[1] + " " + args[2]);
+			System.out.println("Il est " + args[3]);
+		}
+		
+	}
+
+}
