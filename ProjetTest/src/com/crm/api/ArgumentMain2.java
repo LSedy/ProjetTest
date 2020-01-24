5e4f18dc81bba82f564287592b6d8011dcf30124
5Y2 <sedylaurent@gmail.com>
2020-01-24 09:01:13 +0100

    add

--- ProjetTest/src/com/crm/api/ArgumentMain2.java
+++ ProjetTest/src/com/crm/api/ArgumentMain2.java
@@ -13,7 +13,7 @@ public class ArgumentMain2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// 
+		// run configurations 
 		if (args.length == 4) {
 			System.out.println("Welcome" + args[0] + " " + args[1] + " " + args[2]);
 			System.out.println("Il est " + args[3]);
