84f40e0141bff1a26261036486f7fbcc5a4ca86c
5Y2 <sedylaurent@gmail.com>
2020-01-27 08:22:18 +0100

    add thing

---
+++ ProjetTest/src/com/crm/api/SommeElementTableau.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+/**
+ * @author 5Y2
+ *
+ */
+public class SommeElementTableau {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Somme element d'un tableau
+
+	}
+
+}
