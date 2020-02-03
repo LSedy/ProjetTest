c95be2b472df85b71089c8bf9433125944e91393
5Y2 <sedylaurent@gmail.com>
2020-02-03 16:26:53 +0100

    change place of everything

---
+++ ProjetTest/test/com/crm/api/CrashTestAA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+/**
+ * @author 5Y2
+ *
+ */
+public class CrashTestAA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
