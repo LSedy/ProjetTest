1a353a1a4a474a2700e415a18fcbb80c43b6174c
5Y2 <sedylaurent@gmail.com>
2020-02-11 16:40:15 +0100

    work in progress on voiture & voitureapp

--- ProjetTest/test/com/crm/bo/Voiture.java
+++ ProjetTest/test/com/crm/bo/Voiture.java
@@ -10,11 +10,15 @@ package com.crm.bo;
 public class Voiture {
 	
 	//Attributs Marque=Atlas K-3000 Couleur=GrisAnthracyte nBPorte=7 BoiteManu/Auto=Auto Prix=520.000
-	String couleur = "cramoisi ou gris anthracite";
+	String couleur = "gris anthracite";
 	String marque = "Atlas Testarossa";
 	float nbPortes = 7;
 	String auto = "Automatique";
 	int prix = 520000;
+	
+	//attribut tableau/vitesse
+	String[] tabTrad2d = {"50Km/h", "80Km/h", "130Km/h", "225Km/h", "999Km/h"};
+	
 	//ajouter attrubut vitesse
 	
 	//Constructeur
